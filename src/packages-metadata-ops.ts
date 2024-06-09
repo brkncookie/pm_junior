bec49f82649475e1b4bf379a4c376a1f0376b3e9
brkncookie <the0007end@gmail.com>
2024-06-09 16:52:38 +0100

    Modified packages-metadata-ops.ts
    to added the functionality to operate on files such as the lockfile and package.json

---
+++ src/packages-metadata-ops.ts
@@ -0,0 +1,70 @@
+import yargs from "yargs";
+import * as fs from "fs-extra";
+import findUp from "find-up";
+import { Dependencies } from "./packages-ops";
+import * as yaml from "js-yaml";
+
+interface LockEntries {
+  [pkgName: string]: {
+    version: string;
+    url: string;
+    shasum: string;
+    dependencies: Dependencies;
+  };
+}
+
+const toSet: LockEntries = Object.create(null);
+const toGet: LockEntries = Object.create(null);
+
+export function setLock() {
+  fs.writeFileSync("./pm_junior.yml", yaml.dump(toSet, { noRefs: true }));
+}
+
+export function getLock() {
+  fs.pathExistsSync("./pm_junior.yml") &&
+    Object.assign(
+      toGet,
+      yaml.load(fs.readFileSync("./pm_junior.yml", "utf-8"))
+    );
+}
+
+export function setLockEntry(pkgName: string, lockEntry: LockEntries[string]) {
+  toSet[pkgName] = toSet[pkgName] || Object.create(null);
+  Object.assign(toSet[pkgName], lockEntry);
+}
+
+export function getLockEntry(pkgName: string, semanticVersion: string) {
+  const lockEntry = toGet[`${pkgName}@${semanticVersion}`];
+
+  const packageMetadata = lockEntry
+    ? {
+        [lockEntry.version]: {
+          dependencies: lockEntry.dependencies,
+          dist: { shasum: lockEntry.shasum, tarball: lockEntry.url },
+        },
+      }
+    : null;
+
+  return packageMetadata;
+}
+
+export async function getPackageJson(args: yargs.Arguments) {
+  const jsonFile = fs.readJsonSync((await findUp("package.json"))!);
+
+  jsonFile.dependencies = jsonFile.dependencies || {};
+  jsonFile.devDependencies = jsonFile.devDependencies || {};
+
+  const packages = args._.slice(1);
+
+  if (packages.length) {
+    args.dev && packages.forEach((pkg) => (jsonFile.devDependencies[pkg] = ""));
+    !args.dev && packages.forEach((pkg) => (jsonFile.dependencies[pkg] = ""));
+  }
+
+  args.production && delete jsonFile.devDependencies;
+  return jsonFile;
+}
+
+export async function setPackageJson(jsonFile: any) {
+  fs.writeJson((await findUp("package.json"))!, jsonFile, { spaces: 2 });
+}
